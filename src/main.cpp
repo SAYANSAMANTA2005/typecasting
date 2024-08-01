07e5c3c320109e1762eef2f3a670ebde8def38d4
root <root@localhost>
2024-08-01 12:45:33 +0530

    auto publish 1722496533703

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    cout<< "hello world" << endl;
+     
+    
+     int x;
+     cin>>x;
+    cout<<x << endl;
+    cout<< "half of :"<<x<<"=" ;
+     float y=(float)x;
+    cout<< y/2<< endl;
+   
+    
+    
+     
+    return 0;
+}
